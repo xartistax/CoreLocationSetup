42323d9ff8e3c2e8bdf00a6eba7fa6d2ad675a34
Demian Füglistaler <demianfueglistaler@icloud..com>
2023-12-17 19:48:21 +0100

    Initial Commit

---
+++ CoreLocationSetup/CoreLocationSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationSetupApp.swift
+//  CoreLocationSetup
+//
+//  Created by Demian Füglistaler on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
